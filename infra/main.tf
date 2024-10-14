a8fa85251704d695bf1247ab7fe62343ea9c273a
Anna Erdi <erdianna607@gmail.com>
2024-10-14 23:59:06 +0200

    Add terraform configuration files

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+# Configuring the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Setting up the Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Creating the App Service Plan
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+# Creating the Web App
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_app_service_plan.asp.id
+
+  site_config {
+    app_command_line = "npm start"
+  }
+
+  app_settings = {
+    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
+    WEBSITE_RUN_FROM_PACKAGE       = "1"
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  default     = "DevOpsResourceGroup"
+}
+
+variable "location" {
+  description = "Azure Region"
+  default     = "eastus"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  default     = "DevOpsAppServicePlan"
+}
+
+variable "app_name" {
+  description = "Name of the Web App"
+  default     = "my-nextjs-app"
+}
